5e182eb78476eaf946df9fec3810e5d84e746965
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2020-09-24 09:14:42 -0600

    stretch activities team 2 trying

--- routes/ta02.js
+++ routes/ta02.js
@@ -7,6 +7,23 @@ const userArray = ['Jack', 'Jill', 'Brian'];
 // For requirement 02 of TA02
 router.post('/addUser', (req, res, next) => {
     const newUser = req.body.newUser;
+    exist = false;
+    for (var i in userArray){
+        if (exist == false){
+            if (newUser == userArray[i]){
+                exist = true;
+            }
+        }
+    }
+    if (exist == true){
+        res.render('pages/ta02error', {
+            title: 'Team Activity 02',
+            err: 'User already exists',
+            path: '/ta02', // For pug, EJS
+            activeTA03: true, // For HBS
+            contentCSS: true, // For HBS
+        });
+    }
 
     userArray.push(newUser);
 
@@ -16,9 +33,25 @@ router.post('/addUser', (req, res, next) => {
 // For requirement 03 of TA02
 router.post('/removeUser', (req, res, next) => {
     const remUser = req.body.remUser;
-
+    exist = false;
     // Splice method removes from a const array
     const index = userArray.indexOf(remUser);
+    for (var i in userArray){
+        if (exist == false){
+            if (remUser == userArray[i]){
+                exist = true;
+            }
+        }
+    }
+    if (exist == false){
+        res.render('pages/ta02error', {
+            title: 'Team Activity 02',
+            err: 'User not found',
+            path: '/ta02', // For pug, EJS
+            activeTA03: true, // For HBS
+            contentCSS: true, // For HBS
+        });
+    }
     if (index !== -1 ) {
         userArray.splice(index, 1);
     }
